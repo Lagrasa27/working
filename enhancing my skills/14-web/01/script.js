5536f793032c6dfe3f42ac78848f21ee3827a09a
lagrasa27 <jorgebrito2733@gmail.com>
2023-12-25 06:56:45 -0400

    getting there

--- enhancing my skills/14-web/01/script.js
+++ enhancing my skills/14-web/01/script.js
@@ -15,3 +15,9 @@ let email = 'jorgeken@gmail.com';
 let result = email.includes('@');
 
 console.log(result);
+
+let score = '100';
+
+score = Number(score);
+console.log(score+1)
+console.log(typeof score);
