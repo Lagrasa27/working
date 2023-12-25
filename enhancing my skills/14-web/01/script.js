0c4c1c252884b086e98259b0cb8647ec6e53ff73
lagrasa27 <jorgebrito2733@gmail.com>
2023-12-25 06:40:27 -0400

    getting there

--- enhancing my skills/14-web/01/script.js
+++ enhancing my skills/14-web/01/script.js
@@ -9,3 +9,9 @@ console.log(mouse.indexOf('k'));
 let micky = 'exam';
 
 console.log(micky.trim('x'));
+
+let email = 'jorgeken@gmail.com';
+
+let result = email.includes('@');
+
+console.log(result);
