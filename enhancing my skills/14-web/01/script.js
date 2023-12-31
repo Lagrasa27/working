84f7def60e477b8c439f9c51f377ec685f643316
lagrasa27 <jorgebrito2733@gmail.com>
2023-12-30 21:50:28 -0400

    getting there

--- enhancing my skills/14-web/01/script.js
+++ enhancing my skills/14-web/01/script.js
@@ -68,3 +68,21 @@ if(password.length >= 12 ){
 }else {
     console.log('password is not long enought')
 }
+
+//example
+// Convert the provided JavaScript code to ES6
+
+// Define a function named string_reverse with parameter str
+const string_reverse = (str) => {
+    // Use split, reverse, and join methods to reverse the characters of the string
+    return str.split("").reverse().join("");
+};
+
+// Log the result of calling string_reverse with the argument "w3resource" to the console
+console.log(string_reverse("w3resource"));
+
+// Log the result of calling string_reverse with the argument "www" to the console
+console.log(string_reverse("www"));
+
+// Log the result of calling string_reverse with the argument "JavaScript" to the console
+console.log(string_reverse("JavaScript"));
