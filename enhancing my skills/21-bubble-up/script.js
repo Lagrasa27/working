e3346c682af543549970e6c53ebc56854fc4ac86
lagrasa27 <jorgebrito2733@gmail.com>
2024-01-19 21:04:31 -0400

    getting there

--- enhancing my skills/21-bubble-up/script.js
+++ enhancing my skills/21-bubble-up/script.js
@@ -1,5 +1,6 @@
 const button = document.querySelector("button");
 const ul = document.querySelector('ul');
+const cuadro = document.querySelector('.square');
 
 // ul.remove();
 
@@ -7,25 +8,39 @@ button.addEventListener('click', () =>{
     // ul.innerHTML += '<li>something new</li>';
     const li = document.createElement('li');
     li.textContent = 'something new';
-    // ul.prepend(li);
-    ul.append(li);
-
-
+    ul.prepend(li);
+//     ul.append(li);
+//
+//
 });
 
 button.addEventListener('click', () => {
-    console.log('you click the button');
+    // console.log('you click the button');
 });
 
-const list = document.querySelectorAll('li');
 
+/* whe you do a click event if the event is in a children tag it will bubble up to his parent  and if wan to stpo that we can use
+e.stopPropagation();  // this method will stop the click even from bububling up the stopgrogation
+* */
 
+// const list = document.querySelectorAll('li');
+// list.forEach(list =>{
+//     list.addEventListener('click', e =>{
+//         console.log('LI ')
+//         e.stopPropagation();  // this method will stop the click even from bububling up the stopgrogation
+//         // console.log(e.target);
+//         // e.target.style.textDecoration = 'line-through';
+//         // e.target.remove();
+//     });
+// });
 
-list.forEach(list =>{
-    list.addEventListener('click', e =>{
-        console.log('you click the list ')
-        console.log(e.target);
-        e.target.style.textDecoration = 'line-through';
-        // e.target.remove();
-    });
+ul.addEventListener('click', e =>{
+   if ( e.target.tagName === 'LI'){
+   }
+    // e.target.remove();
+});
+
+cuadro.addEventListener('mousemove', (e) =>{
+    // console.log(e);
+    cuadro.textContent = `x pos - ${e.offsetX} y pos - ${e.offsetY}`;
 });
