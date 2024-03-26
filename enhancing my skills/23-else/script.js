95a5ce8cfb49415b2ef63c9d38916dead0aed5e9
Jorge Elias Brito de la Cruz <71567819+Lagrasa27@users.noreply.github.com>
2024-03-26 10:11:36 -0400

    wheather proyect completed.

---
+++ enhancing my skills/23-else/script.js
@@ -0,0 +1,9 @@
+const pass = 'mario and ken';
+
+if (pass.length >= 14){
+    console.log('password is valid and strong');
+} else if (pass.length >= 8) {
+    console.log( 'password is valid ')
+} else {
+    console.log('password is not valid')
+}
