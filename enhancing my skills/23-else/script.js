21bcb31581e410940119a0b436abbcac66db02ee
Jorge Elias Brito de la Cruz <71567819+Lagrasa27@users.noreply.github.com>
2024-03-26 13:35:13 -0400

    wheather proyect completed.

--- enhancing my skills/23-else/script.js
+++ enhancing my skills/23-else/script.js
@@ -15,3 +15,33 @@ if (positive){
 }
 
 console.log(false);
+// this is the switch statment work very well y you can add additional case
+
+let vehicle = 'b'
+
+switch (vehicle){
+    case "A,","a":
+        console.log('new vehicle');
+        break
+    case "B","b":
+        console.log(' mint vehicle');
+        break
+    case "C":
+        console.log('very god vehicle');
+        break
+    case "D":
+        console.log('good vehicle');
+        break
+    case "E":
+        console.log('use vehicle');
+        break
+    case "F":
+        console.log('very use vehicle');
+        break
+    case "G":
+        console.log('use many miles vehicle');
+        break
+    default:
+        console.log('vehicle no worth it');
+
+}
