cabb3a8be74896a1b30d29f459f7ba3aca97f13d
Jorge Elias Brito de la Cruz <71567819+Lagrasa27@users.noreply.github.com>
2024-04-09 16:14:27 -0400

    wheather proyect completed.

---
+++ enhancing my skills/26-objetc/java.js
@@ -0,0 +1,18 @@
+let user = {
+    name: 'John Smith',
+    email: 'johnSmith@gmail.com',
+    age: 30,
+    location: 'New York',
+    blog: ['estamos aqui aprendiendo', 'hacer cosas nuevas'],
+
+};
+
+// in order to acces to specific value at objet you can use dot .
+// or sqare bracket but must be in string
+
+
+
+console.log(user);
+console.log(user.name);
+console.log(user['location']);
+console.log(typeof user);
