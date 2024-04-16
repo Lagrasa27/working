41bc2903c285ef9f1cc65524781226fc9e31a222
Jorge Elias Brito de la Cruz <71567819+Lagrasa27@users.noreply.github.com>
2024-04-16 14:31:11 -0400

    wheather proyect completed.

---
+++ enhancing my skills/27-event/02/java.js
@@ -0,0 +1,26 @@
+const ulist = document.querySelector('ul');
+
+const list = document.querySelectorAll('li');
+
+const boton = document.querySelector('button');
+
+console.log(list)
+
+
+
+list.forEach((lista) =>{
+    lista.addEventListener('click', (e)=>{
+        console.log(e.target);
+    })
+
+})
+
+boton.addEventListener('click', ()=>{
+    //ul.innerHTML += ' <li> something new </li> you can do it this way as well
+   const li = document.createElement('li');
+   li.textContent = 'here we go!';
+   // ulist.append(li);
+    ulist.prepend(li);
+});
+
+
