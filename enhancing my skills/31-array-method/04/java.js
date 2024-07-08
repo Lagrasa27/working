e16b6d13489d877e2aa3dd19938f75f512608a1a
Jorge Elias Brito de la Cruz <71567819+Lagrasa27@users.noreply.github.com>
2024-07-08 19:26:56 -0400

    wheather proyect completed.

--- enhancing my skills/31-array-method/04/java.js
+++ enhancing my skills/31-array-method/04/java.js
@@ -3,3 +3,8 @@ const scores = [ 10,30,40,35,85,20,66,];
 const  lookFirstresult = scores.find(score => score > 50);
 
 console.log(lookFirstresult);
+
+const paraf = document.querySelector('p');
+
+paraf.innerText = 'retomando el ritmo';
+
