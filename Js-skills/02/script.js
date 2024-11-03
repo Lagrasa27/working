fb38606fcc0ce79ea29cfa2699e8e912d67c08b0
Jorge Elias Brito de la Cruz <71567819+Lagrasa27@users.noreply.github.com>
2024-11-03 08:41:24 -0400

    code academy.

--- Js-skills/02/script.js
+++ Js-skills/02/script.js
@@ -16,3 +16,19 @@ function nombre (a){
 
 let result = nombre( "jorge")
 console.log(result)
+
+function numbers (a,b){
+    return a+b
+}
+
+let algo =  numbers(10,30)
+console.log(algo)
+
+
+function metas (aprender, aplicar ){
+    return aprender + aplicar
+
+}
+
+let result2 = metas('aplicar estas enseñanasas ', "klk " )
+console.log(result2)
