f08d8389ec144734e3fe8bd8edb4adabfcd59a9f
Jorge Elias Brito de la Cruz <71567819+Lagrasa27@users.noreply.github.com>
2024-11-03 08:08:24 -0400

    code academy.

---
+++ Js-skills/02/script.js
@@ -0,0 +1,18 @@
+function name () {
+    console.log('Jorge Brito')
+}
+
+function personName (mujer, hombre) {
+    console.log(mujer, hombre)
+}
+
+personName('gicely', 'Juan')
+
+name();
+
+function nombre (a){
+    return 'hello ' +  a
+}
+
+let result = nombre( "jorge")
+console.log(result)
