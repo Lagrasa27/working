58f8514f952f06426f991b087547347988cc256d
Jorge Elias Brito de la Cruz <71567819+Lagrasa27@users.noreply.github.com>
2025-03-28 20:54:09 -0400

    code academy.

---
+++ Js-skills/05/script.js
@@ -0,0 +1,30 @@
+let a = 1
+let b = 2
+
+console.log(a === b);
+
+console.log(a !==b);
+
+function name() {
+    console.log('jorge')
+}
+
+name()
+function tifa(name){
+    console.log(name)
+}
+
+tifa('jorge elias')
+
+function number(mul, sum){
+    console.log(mul + sum)
+}
+
+number(10, 20)
+
+function printName(name1){
+    return "hello" + name1
+}
+
+let name2 = printName('jorge')
+console.log(name2)
