2751111a8582eed76c3c17e0b9d5b8d930d27cb0
Jorge Elias Brito de la Cruz <71567819+Lagrasa27@users.noreply.github.com>
2025-09-15 09:40:41 -0400

    skills

---
+++ Js-skills/13-simple-to-do-list/script.js
@@ -0,0 +1,28 @@
+// 1.select all element
+const form = document.querySelector('#new-item-form');
+const list = document.querySelector('#list');
+const input = document.querySelector('#item-input');
+//2. when I submit the form add a new element
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // 1. create a new item
+const item = document.createElement('div')
+    item.innerText = input.value
+    item.classList.add('list-item')
+
+   // 2. add that item to the list
+list.appendChild(item)
+
+    //3. clear input
+    input.value = '';
+ // 4. setup event listener to delete item when clicked
+item.addEventListener('click', () => {
+    list.removeChild(item)
+})
+})
+
+
+
+K
